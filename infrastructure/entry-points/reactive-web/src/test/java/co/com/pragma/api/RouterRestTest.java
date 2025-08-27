e48d0ce3d1c3a894864f93b5b08f18e4e75cd953
diego <diego1853@gmail.com>
2025-08-27 07:49:38 -0500

    cubrimiento tests unitarios

--- infrastructure/entry-points/reactive-web/src/test/java/co/com/pragma/api/RouterRestTest.java
+++ infrastructure/entry-points/reactive-web/src/test/java/co/com/pragma/api/RouterRestTest.java
@@ -1,12 +1,34 @@
 package co.com.pragma.api;
 
+import co.com.pragma.api.dto.CreateUserDto;
+import co.com.pragma.api.dto.UserResponseDto;
+import co.com.pragma.api.mapper.UserMapperDto;
+import co.com.pragma.model.user.User;
+import co.com.pragma.model.user.valueObjects.Email;
+import co.com.pragma.model.user.valueObjects.LastName;
+import co.com.pragma.model.user.valueObjects.Name;
+import co.com.pragma.model.user.valueObjects.SalaryBase;
+import co.com.pragma.usecase.user.UserUseCase;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+import static org.mockito.ArgumentMatchers.any;
+import static reactor.core.publisher.Mono.when;
 
 @ContextConfiguration(classes = {RouterRest.class, Handler.class})
 @WebFluxTest
@@ -15,45 +37,65 @@ class RouterRestTest {
     @Autowired
     private WebTestClient webTestClient;
 
-    @Test
-    void testListenGETUseCase() {
-        webTestClient.get()
-                .uri("/api/usecase/path")
-                .accept(MediaType.APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(String.class)
-                .value(userResponse -> {
-                            Assertions.assertThat(userResponse).isEmpty();
-                        }
-                );
-    }
 
-    @Test
-    void testListenGETOtherUseCase() {
-        webTestClient.get()
-                .uri("/api/otherusercase/path")
-                .accept(MediaType.APPLICATION_JSON)
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody(String.class)
-                .value(userResponse -> {
-                            Assertions.assertThat(userResponse).isEmpty();
-                        }
-                );
+    @MockitoBean
+    private UserUseCase userUseCase;
+
+    @MockitoBean
+    private UserMapperDto userMapperDto;
+
+    private final String users = "/api/v1/usuarios";
+
+    private final Clock fixedClock = Clock.fixed(
+            LocalDate.of(2025, 8, 27).atStartOfDay(ZoneId.systemDefault()).toInstant(),
+            ZoneId.systemDefault()
+    );
+
+    private final User user = User.builder()
+            .idUser(1L)
+            .idDocument("1234567")
+            .name(new Name("name"))
+            .lastname(new LastName("lastName"))
+            .birthdate(LocalDate.now(fixedClock))
+            .address("address")
+            .mobile("3199689469")
+            .email(new Email("email@email.com"))
+            .salaryBase(new SalaryBase(BigDecimal.valueOf(1)))
+            .idRole(1L)
+            .build();
+
+    private final CreateUserDto createUserDto = new CreateUserDto("1234567",
+            "Name",
+            "lastName",
+            LocalDate.now(fixedClock),
+            "address",
+            "3199689469",
+            "email@email.com",
+            BigDecimal.valueOf(1),
+            1L);
+
+    private final UserResponseDto responseDto = new UserResponseDto(user.getIdUser());
+
+    @BeforeEach
+    void setup() {
+        Mockito.when(userMapperDto.toModel(any(CreateUserDto.class))).thenReturn(user);
+        Mockito.when(userUseCase.saveUser(any(User.class))).thenReturn(Mono.just(user));
+        Mockito.when(userMapperDto.toResponse(any(User.class))).thenReturn(responseDto);
     }
 
     @Test
     void testListenPOSTUseCase() {
+
+
         webTestClient.post()
-                .uri("/api/usecase/otherpath")
+                .uri(users)
                 .accept(MediaType.APPLICATION_JSON)
-                .bodyValue("")
+                .bodyValue(createUserDto)
                 .exchange()
                 .expectStatus().isOk()
-                .expectBody(String.class)
+                .expectBody(UserResponseDto.class)
                 .value(userResponse -> {
-                            Assertions.assertThat(userResponse).isEmpty();
+                            Assertions.assertThat(userResponse.getIdUser()).isEqualTo(user.getIdUser());
                         }
                 );
     }
--- infrastructure/entry-points/reactive-web/src/test/java/co/com/pragma/api/config/ConfigTest.java
+++ infrastructure/entry-points/reactive-web/src/test/java/co/com/pragma/api/config/ConfigTest.java
@@ -2,12 +2,33 @@ package co.com.pragma.api.config;
 
 import co.com.pragma.api.Handler;
 import co.com.pragma.api.RouterRest;
+import co.com.pragma.api.dto.CreateUserDto;
+import co.com.pragma.api.dto.UserResponseDto;
+import co.com.pragma.api.mapper.UserMapperDto;
+import co.com.pragma.model.user.User;
+import co.com.pragma.model.user.valueObjects.Email;
+import co.com.pragma.model.user.valueObjects.LastName;
+import co.com.pragma.model.user.valueObjects.Name;
+import co.com.pragma.model.user.valueObjects.SalaryBase;
+import co.com.pragma.usecase.user.UserUseCase;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ContextConfiguration(classes = {RouterRest.class, Handler.class})
 @WebFluxTest
@@ -17,14 +38,61 @@ class ConfigTest {
     @Autowired
     private WebTestClient webTestClient;
 
+    @MockitoBean
+    private UserUseCase userUseCase;
+
+    @MockitoBean
+    private UserMapperDto userMapperDto;
+
+    private final Clock fixedClock = Clock.fixed(
+            LocalDate.of(2025, 8, 27).atStartOfDay(ZoneId.systemDefault()).toInstant(),
+            ZoneId.systemDefault()
+    );
+
+    private final User user = User.builder()
+            .idUser(1L)
+            .idDocument("1234567")
+            .name(new Name("name"))
+            .lastname(new LastName("lastName"))
+            .birthdate(LocalDate.now(fixedClock))
+            .address("address")
+            .mobile("3199689469")
+            .email(new Email("email@email.com"))
+            .salaryBase(new SalaryBase(BigDecimal.valueOf(1)))
+            .idRole(1L)
+            .build();
+
+    private final CreateUserDto createUserdto = new CreateUserDto("1234567",
+            "Name",
+            "lastName",
+            LocalDate.now(fixedClock),
+            "address",
+            "3199689469",
+            "email@email.com",
+            BigDecimal.valueOf(1),
+            1L);
+
+    private final UserResponseDto responseDto = new UserResponseDto(user.getIdUser());
+
+
+    @BeforeEach
+    void setup() {
+        when(userMapperDto.toModel(any(CreateUserDto.class))).thenReturn(user);
+        when(userUseCase.saveUser(any(User.class))).thenReturn(Mono.just(user));
+        when(userMapperDto.toResponse(any(User.class))).thenReturn(responseDto);
+    }
+
     @Test
     void corsConfigurationShouldAllowOrigins() {
-        webTestClient.get()
-                .uri("/api/usecase/path")
+
+
+        webTestClient.post()
+                .uri("/api/v1/usuarios")
+                .bodyValue(createUserdto)
                 .exchange()
                 .expectStatus().isOk()
                 .expectHeader().valueEquals("Content-Security-Policy",
-                        "default-src 'self'; frame-ancestors 'self'; form-action 'self'")
+                "default-src 'self'; frame-ancestors 'self'; form-action 'self'")
                 .expectHeader().valueEquals("Strict-Transport-Security", "max-age=31536000;")
                 .expectHeader().valueEquals("X-Content-Type-Options", "nosniff")
                 .expectHeader().valueEquals("Server", "")
